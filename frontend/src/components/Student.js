443ed8107ce6f8e5854a96b19474d20bde546ce2
Personal Blitz <iis_dbga@live.com>
2023-10-26 10:59:01 -0600

    project finished!

--- frontend/src/components/Student.js
+++ frontend/src/components/Student.js
@@ -3,31 +3,39 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container } from '@mui/material';
 import {Paper} from '@mui/material';
-import { useState } from 'react';
-import Stack from '@mui/material/Stack';
+import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 
 export default function Student() {
     
     const paperStyle= {padding: '50px 20px', width:600, margin:"20px auto" }
-    const [name, setName] = useState("")
-    const [address, setAddress] = useState("")
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [students, setStudents]=useState("")
     const handleClick=(e)=>{
         e.preventDefault()
         const student={name, address}
         fetch("http://localhost:8080/student/add",{
             method: "POST",
-            headers:{"Content-type":"application.json"},
+            headers:{"Content-type":"application/json"},
             body:JSON.stringify(student)
         
     }).then(()=>{
         console.log("New student added")
     })
     }
+
+    useEffect (()=>{
+      fetch("http://localhost:8080/student/getAll")
+      .then(res => res.json())
+      .then((result)=>{
+        setStudents(result)
+        })
+      },[])
+  
   return (
     <Container>
         <Paper elevation={3} style={paperStyle}>
-
             <h1 style={{color:"blue"}}><u>Add a Student</u></h1>
     <Box
       component="form"
@@ -49,9 +57,19 @@ export default function Student() {
       onClick={handleClick}
       >Submit</Button>
     </Box>
-    {name}
-      {address}
+   
     </Paper>
+    <Paper elevation={3} style={paperStyle}>
+      {students.map(student =>(
+        <Paper elecation={6} style={{margin:"10px", padding:"15px", textAlign:"left"}} key={student.id}>
+          ID:{student.id}<br/>
+          Name:{student.name}<br/>
+          Address:{student.adress}
+        </Paper>
+      ))}
+        
+    </Paper>
+
     </Container>
   );
 }
